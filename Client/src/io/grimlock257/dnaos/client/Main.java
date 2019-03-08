679a57bf4649f24475e1d9d1e8c4bbd5f251d4e5
Adam Watson <adamwatsonoa@gmail.com>
2019-03-08 11:58:21 +0000

    Initial commit
    Initial structure of projects

---
+++ Client/src/io/grimlock257/dnaos/client/Main.java
@@ -0,0 +1,14 @@
+package io.grimlock257.dnaos.client;
+
+/**
+ * Main class of Client project
+ *
+ * Adam Watson
+ * Year 2 - Computer Systems Engineering
+ * Distributed Network Architecture & Operating Systems Module CW-2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am client");
+    }
+}
---
+++ Node/src/io/grimlock257/dnaos/node/Main.java
@@ -0,0 +1,14 @@
+package io.grimlock257.dnaos.node;
+
+/**
+ * Main class of Node project
+ *
+ * Adam Watson
+ * Year 2 - Computer Systems Engineering
+ * Distributed Network Architecture & Operating Systems Module CW-2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am node");
+    }
+}
