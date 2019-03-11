9dff39827c5ce9b63884017ef05ca3f58fe3a073
Adam Watson <adamwatsonoa@gmail.com>
2019-03-11 21:16:04 +0000

    Add planned MessageType enums

---
+++ Node/src/io/grimlock257/dnaos/node/message/MessageType.java
@@ -0,0 +1,10 @@
+package io.grimlock257.dnaos.node.message;
+
+public enum MessageType {
+    NODE_REGISTER,
+    NODE_RESIGN,
+    NEW_JOB,
+    COMPLETE_JOB,
+    LB_SHUTDOWN,
+    NODE_SHUTDOWN
+}
