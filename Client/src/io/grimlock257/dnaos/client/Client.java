8d36f57c50b9a5d6f7642a807254153d489eb7d3
Adam Watson <adamwatsonoa@gmail.com>
2019-04-16 01:28:14 +0100

    User input tidy up

--- Client/src/io/grimlock257/dnaos/client/Client.java
+++ Client/src/io/grimlock257/dnaos/client/Client.java
@@ -47,6 +47,10 @@ public class Client {
     private MessageManager messageManager;
     private JobManager jobManager;
 
+    // Keyboard access for user input
+    InputStreamReader input;
+    BufferedReader keyboard;
+
     /**
      * Create a new client instance
      *
@@ -74,6 +78,9 @@ public class Client {
             messageManager = MessageManager.getInstance();
             messageManager.init(socket);
             jobManager = JobManager.getInstance();
+
+            input = new InputStreamReader(System.in);
+            keyboard = new BufferedReader(input);
 
             // Have setup first?
             loop();
@@ -229,111 +236,184 @@ public class Client {
      * Get input from the user search as the command they want to issue, and any required arguments
      */
     private void getUserInput() {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader keyboard = new BufferedReader(input);
-
         System.out.println("===============================================================================");
 
-        // Ask the user for command information
-        try {
-            // Ask what type of command to carry out
-            System.out.println("What command do you want to issue (Enter command number)?");
-            for (CommandOptions option : EnumSet.allOf(CommandOptions.class)) {
-                System.out.println(option.ordinal() + 1 + ") " + option.toString().replace("_", " "));
-            }
+        // Ask what type of command to carry out
+        System.out.println("What command do you want to issue (Enter command number)?");
+        for (CommandOptions option : EnumSet.allOf(CommandOptions.class)) {
+            System.out.println(option.ordinal() + 1 + ") " + option.toString().replace("_", " "));
+        }
 
-            System.out.print("> ");
-            int menuSelection = Integer.parseInt(keyboard.readLine());
+        int menuSelection = getMenuInput(EnumSet.allOf(CommandOptions.class).size());
 
-            // Convert their number selection back to the enum value
-            CommandOptions selected = CommandOptions.values[menuSelection - 1];
+        // Convert their number selection back to the enum value
+        CommandOptions selected = CommandOptions.values[menuSelection - 1];
 
-            // Provide the relevant input form depending on the menu option they selected
-            switch (selected) {
-                case NEW_JOB:
-                    System.out.print("Enter new job name: ");
-                    String jobName = keyboard.readLine(); // TODO: Validation // TODO: Duplicate check
+        // Provide the relevant input form depending on the menu option they selected
+        switch (selected) {
+            case NEW_JOB:
+                System.out.print("Enter new job name: ");
+                String jobName = getStringInput();
 
-                    System.out.print("Enter new job duration: ");
-                    int jobDuration = Integer.parseInt(keyboard.readLine());
-                    Job newJob = new Job(jobName, jobDuration);
+                System.out.print("Enter new job duration: ");
+                int jobDuration = getIntegerInput();
+                Job newJob = new Job(jobName, jobDuration);
 
-                    boolean hasJobAdded = jobManager.addJob(newJob);
+                boolean hasJobAdded = jobManager.addJob(newJob);
 
-                    if (!hasJobAdded) {
-                        System.out.println("[ERROR] Job was not added, the supplied information matched an existing job\n");
+                if (!hasJobAdded) {
+                    System.out.println("[ERROR] Job was not added, the supplied information matched an existing job\n");
+                } else {
+                    messageManager.send(MessageType.NEW_JOB.toString() + "," + jobName + "," + jobDuration, lbAddr, lbPort);
+
+                    System.out.println("\n[INFO] New job added: " + newJob.toString() + "\n");
+                    System.out.println("[INFO] Current job list:\n" + jobManager.toString());
+                }
+
+                break;
+            case CANCEL_JOB:
+                System.out.print("Enter job name of which to cancel: ");
+                String cancelJobName = getStringInput();
+
+                if (cancelJobName == null) {
+                    System.out.println("[ERROR] Cancel job request was not sent, some of the supplied information was invalid");
+                } else {
+                    Job cancelJob = jobManager.findByName(cancelJobName);
+
+                    if (cancelJob == null) {
+                        System.out.println("[ERROR] Cancel job request was not issued as no job with name '" + cancelJobName + "' was found");
+                    } else if (jobManager.getJobStatus(cancelJobName) == JobStatus.COMPLETE) {
+                        System.out.println("[ERROR] Cancel job request was not sent as the job is already complete");
                     } else {
-                        messageManager.send(MessageType.NEW_JOB.toString() + "," + jobName + "," + jobDuration, lbAddr, lbPort);
+                        jobManager.updateJobStatus(cancelJob, JobStatus.REQUESTED_CANCEL);
 
-                        System.out.println("\n[INFO] New job added: " + newJob.toString() + "\n");
-                        System.out.println("[INFO] Current job list:\n" + jobManager.toString());
+                        messageManager.send(MessageType.CANCEL_JOB_REQUEST.toString() + "," + cancelJobName, lbAddr, lbPort);
+
+                        System.out.println("\n[INFO] Job cancel request has been issued");
                     }
+                }
 
-                    break;
-                case CANCEL_JOB:
-                    System.out.print("Enter job name of which to cancel: ");
-                    String cancelJobName = keyboard.readLine(); // TODO: Validation // TODO: Duplicate check
+                break;
+            case SHUTDOWN_ALL:
+                System.out.print("Are you sure you want to shutdown this client, the load balancer and all nodes? (Y/N): ");
 
-                    if (cancelJobName == null) {
-                        System.out.println("[ERROR] Cancel job request was not sent, some of the supplied information was invalid");
-                    } else {
-                        Job cancelJob = jobManager.findByName(cancelJobName);
+                String selection = getStringInput().toLowerCase();
 
-                        if (cancelJob == null) {
-                            System.out.println("[ERROR] Cancel job request was not issued as no job with name '" + cancelJobName + "' was found");
-                        } else if (jobManager.getJobStatus(cancelJobName) == JobStatus.COMPLETE) {
-                            System.out.println("[ERROR] Cancel job request was not sent as the job is already complete");
-                        } else {
-                            jobManager.updateJobStatus(cancelJob, JobStatus.REQUESTED_CANCEL);
+                while (!selection.equals("y") && !selection.equals("n")) {
+                    System.out.println("[INPUT ERROR] Please enter either Y or N");
+                    System.out.print("> ");
 
-                            messageManager.send(MessageType.CANCEL_JOB_REQUEST.toString() + "," + cancelJobName, lbAddr, lbPort);
+                    selection = getStringInput().toLowerCase();
+                }
 
-                            System.out.println("\n[INFO] Job cancel request has been issued");
-                        }
-                    }
+                if (selection.equals("y")) {
+                    messageManager.send(MessageType.LB_SHUTDOWN.toString(), lbAddr, lbPort);
 
-                    break;
-                case SHUTDOWN_ALL:
-                    System.out.print("Are you sure you want to shutdown the load balancer and all nodes? (Y/N): ");
+                    System.out.println("[INFO] Shutting down...");
+                    System.exit(0);
+                } else {
+                    System.out.println("[INFO] Shutdown cancelled");
+                }
 
-                    String selection = keyboard.readLine().toLowerCase();
+                break;
+            case SHUTDOWN_SPECIFIC:
+                System.out.print("Enter node name to shutdown: ");
 
-                    while (!selection.equals("y") && !selection.equals("n")) {
-                        System.out.println("[INPUT ERROR] Please enter either Y or N");
-                        System.out.print("> ");
+                String nodeToShutdown = getStringInput();
 
-                        selection = keyboard.readLine().toLowerCase();
-                    }
+                messageManager.send(MessageType.NODE_SHUTDOWN_SPECIFIC.toString() + "," + nodeToShutdown, lbAddr, lbPort);
+                System.out.println("");
 
-                    if (selection.equals("y")) {
-                        // TODO: Disconnect Client after sending LB_SHUTDOWN
-                        messageManager.send(MessageType.LB_SHUTDOWN.toString(), lbAddr, lbPort);
-                        System.out.println("\n[INFO] Current job list:\n" + jobManager.toString() + "\n");
-                    } else {
-                        System.out.println("[INFO] Shutdown cancelled");
-                    }
+                // TODO: Get feedback (success boolean) ?
 
-                    break;
-                case SHUTDOWN_SPECIFIC:
-                    System.out.print("Enter node name to shutdown: ");
-
-                    String nodeToShutdown = keyboard.readLine(); // TODO: Validation // TODO: Duplicate check
-
-                    messageManager.send(MessageType.NODE_SHUTDOWN_SPECIFIC.toString() + "," + nodeToShutdown, lbAddr, lbPort);
-                    System.out.println("");
-
-                    // TODO: Get feedback (success boolean) ?
-
-                    break;
-                default:
-                    System.err.println("[ERROR] Something went wrong... unknown option '" + selected.toString() + "'");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("[INPUT ERROR] Please enter an integer only");
-        } catch (Exception e) {
-            e.printStackTrace();
+                break;
+            default:
+                System.err.println("[ERROR] Something went wrong... unknown option '" + selected.toString() + "'");
         }
     }
+
+    /**
+     * Get validated menu input from a user, between 1 and specified maximum
+     *
+     * @param maxMenuOption The maximum integer allowed
+     *
+     * @return The validated menu option
+     */
+    private int getMenuInput(int maxMenuOption) {
+        int menuSelection = -1;
+
+        while (menuSelection < 1 || menuSelection > maxMenuOption) {
+            try {
+                System.out.print("> ");
+                menuSelection = Integer.parseInt(keyboard.readLine());
+
+                if (menuSelection < 1 || menuSelection > EnumSet.allOf(CommandOptions.class).size()) {
+                    System.out.println("[INPUT ERROR] Please enter integer between 1 and " + maxMenuOption);
+                } else {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("[INPUT ERROR] Please enter an integer only");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return menuSelection;
+    }
+
+    /**
+     * Get validated integer input from the user (i.e integer input that is 0 or greater)
+     *
+     * @return The validated integer input
+     */
+    private int getIntegerInput() {
+        int userInput;
+
+        while (true) {
+            try {
+                userInput = Integer.parseInt(keyboard.readLine());
+
+                if (userInput < 0) {
+                    System.out.println("[INPUT ERROR] Please enter an integer that is 0 or greater");
+                } else {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("[INPUT ERROR] Please enter an integer only");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return userInput;
+    }
+
+    /**
+     * Get validated string input from the user (i.e input with a length greater than 0)
+     *
+     * @return The validated string
+     */
+    private String getStringInput() {
+        String userInput = "";
+
+        while (userInput.length() < 1) {
+            try {
+                userInput = keyboard.readLine();
+
+                if (userInput.length() < 1) {
+                    System.out.println("[INPUT ERROR] Please enter a string with a length greater than 1");
+                } else {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return userInput;
+    }
+
 
     /**
      * Validate the MessageType of the message
